59c42a6cd0915a17496ce8c49f892259ec3f717b
HiRustem <rustemxrid@icloud.com>
2025-01-01 17:33:52 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "./styles/normalize.scss";
-import "./styles/variables.scss";
+import "./styles/normalize.css";
+import "./styles/variables.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
