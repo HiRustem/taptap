8dc22c51edded67568264206042735c4595109ed
HiRustem <rustemxrid@icloud.com>
2025-01-01 16:57:57 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), svgr(), tsconfigPaths(), vercel()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
